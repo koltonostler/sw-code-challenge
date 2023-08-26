9ca4fa80c30cc28e65b90620ee99c11ee4d6d621
koltonostler <95270713+koltonostler@users.noreply.github.com>
2023-08-25 22:45:20 -0600

    Setup Database
    Setup Migrations for the database as well as seeding with dummy data for testing during development.

--- backend/database/factories/ArticleFactory.php
+++ backend/database/factories/ArticleFactory.php
@@ -4,11 +4,12 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use App\Models\User;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
-class UserFactory extends Factory
+class ArticleFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -18,9 +19,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'isAuthorized' => fake()->boolean(50),
+            'title' => 'Title ' . fake()->text(8),
+            'body' => fake()->paragraph(),
+            'user_id' => User::all()->random()->id,
         ];
     }
-
 }
